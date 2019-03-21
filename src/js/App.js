c13cbb7e59f53aae9c31f644b141008eb62c188a
HTMLhead <mrdoo309@gmail.com>
2019-03-21 20:26:47 +0900

    Feat: src폴더 내부 변경:sass사용, 필요없는 파일 제거

---
+++ src/js/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../scss/app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='todo-app'>HI</div>
+    );
+  }
+}
+
+export default App;
