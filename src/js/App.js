3a1ea026500fb8c044598ef7737a7692a7e57b2e
HTMLhead <mrdoo309@gmail.com>
2019-03-21 21:20:00 +0900

    Feat: json데이터를 가져오는 부분 제작

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import '../scss/app.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      task: null,
+    }
+  }
+
+  componentDidMount() {
+    this.getData('https://javascript-web-todo-server.herokuapp.com/')
+  }
+
+  getData(dataUrl) {
+    fetch(dataUrl).then(res => {
+      return res.json()
+    }).then(jsonData => {
+      this.handleData(jsonData);
+    }) 
+  }
+
+  handleData(jsonData) {
+    this.setState({
+      task: jsonData
+    })
+  }
+  
   render() {
     return (
-      <div className='todo-app'>HI</div>
+      <div className='todo-app'>{}</div>
     );
   }
 }
