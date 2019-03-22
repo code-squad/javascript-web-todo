3a79c997ef265227d27534e7778dd8bf9dfd5ad0
Kim <a01088570194@gmail.com>
2019-03-22 17:03:06 +0900

    [Add] list items 생성하는createList

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,22 @@
 export default class TodoList extends React.Component{
-    constructor(){
+    constructor(props){
+        super(props);
         this.state={
             isOpen: true
         }
+    }
+    componentDidMount(){
+        this.createList(this.props);
+    }
+    createList(props){
+        if(toString.call(props.list) !== '[object Array]') return;
+        const listItems = props.list.map(item => 
+            <li key={item.id} onClick={this.removeItem}>
+                {item.title}
+                <button type="button">X</button>
+            </li>
+        )
+        return listItems;
     }
     
 }
