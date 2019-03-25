64ace6f530a7d9020fb08dbbeb64c05ea2e7771d
Head <40166539+HTMLhead@users.noreply.github.com>
2019-03-25 12:32:10 +0900

    [head] Step2. 할일 추가/삭제 (#8)
    * Docs: 이전의 README를 제거하고 새로운 README작성
    
    * Refactor: fetch대신 async/await를 사용, try catch사용
    
    * Feat: Loading돔 제작
    
    * Feat: 삭제하는 버튼 제작
    
    * Refactor: 접는 버튼 css수정
    
    * Refactor: input쪽 css수정, container들 css 수정
    
    * Refactor: 크기, 간격조정
    
    * Feat: 할 일을 추가하는 기능 완성
    
    * Feat: 할 일 제거 기능 추가
    
    * Feat: 같은 할일 입력시 경고문이 나오도록 추가
    
    * Feat: 같은 할일 입력시 경고문의 dom추가
    
    * Feat: 할일이 추가되면 Input.value가 공백이 되는 기능 추가
    
    * Style: 컨벤션에 맞도록 라인변경
    
    * Style: 컨벤션에 맞도록 라인변경
    
    * Feat: 경고문 css추가
    
    * Feat: 경고문 css app쪽으로 import
    
    * Feat: 배경색 추가
    
    * Feat: 숨기는 애니메이션 추가
    
    * Feat: Enter를 눌렀을 시 할일이 추가되는 기능 추가
    
    * Refactor: 경고문 색깔 변경
    
    * Fix: 공백을 추가했을 때 할일이 지워지지 않던 버그 수정

--- src/js/App.js
+++ src/js/App.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import '../scss/app.css';
-
-function MakeTaskDom(props) {
-  return (
-    <div className="task">
-      {props.data.map((v, i) => {
-        return <li key={i} className="task-list">{v.title}</li>;
-      })}
-    </div>
-  );
-}
+import { MakeTaskDom, MakeLoadingDom, MakeWarningDom } from './make-dom.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      task: null,
+      IDNum: 0,
+      tasks: null,
+      word: '',
     };
   }
 
@@ -23,37 +17,108 @@ class App extends Component {
     this.getData('https://javascript-web-todo-server.herokuapp.com/');
   }
 
-  getData(dataUrl) {
-    fetch(dataUrl)
-      .then(res => {
-        return res.json();
-      })
-      .then(jsonData => {
-        this.handleData(jsonData);
-      });
+  async getData(dataUrl) {
+    try {
+      const fetchedRes = await fetch(dataUrl);
+      const jsonData = await fetchedRes.json();
+      this.handleData(jsonData);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  handleData(jsonData) {
+  handleData = jsonData => {
     this.setState({
-      task: jsonData,
+      tasks: jsonData,
     });
-  }
+  };
 
-  renderTaskDom() {
-    if (this.state.task === null) return;
-    return <MakeTaskDom data={this.state.task} />;
-  }
+  renderWarning = tasks => {
+    if (tasks === null)
+      return <div className="warning">잠시만 기다려주세요.</div>;
+    if (tasks.some(v => v.title === this.state.word))
+      return <MakeWarningDom className="warning" />;
+    return;
+  };
+
+  renderTaskDom = () => {
+    if (this.state.tasks === null)
+      return <MakeLoadingDom className="loading" />;
+    return <MakeTaskDom data={this.state.tasks} removeTask={this.removeTask} />;
+  };
+
+  handleKeyPress = e => {
+    if (e.key === 'Enter') this.addTask(e);
+  };
+
+  handleChange = e => {
+    this.setState({
+      word: e.target.value,
+    });
+  };
+
+  addTask = e => {
+    const tasks = this.state.tasks.slice();
+    let IDNumber = this.state.IDNum;
+    if (tasks.some(v => v.title === this.state.word)) return;
+    tasks.push({
+      title: `${this.state.word}`,
+      id: `${IDNumber}`,
+      status: 'todo',
+    });
+
+    this.setState({
+      word: '',
+      tasks: tasks,
+      IDNum: IDNumber + 1,
+    });
+  };
+
+  removeTask = e => {
+    const tasks = this.state.tasks.slice();
+    let title = e.target.parentNode.innerText.split('\n')[0];
+    if(title === '제거') title = ''
+    const removedTask = tasks.filter(task => {
+      return !(task.title === title);
+    });
+
+    this.setState({
+      tasks: removedTask,
+    });
+  };
+
+  hideTask = e => {
+    const btnDom = e.target;
+    if (btnDom.innerText === '접기') {
+      btnDom.innerText = '열기';
+      btnDom.parentNode.classList.add('hide');
+    } else {
+      btnDom.innerText = '접기';
+      btnDom.parentNode.classList.remove('hide');
+    }
+  };
 
   render() {
     return (
       <div className="todo-app-conatiner">
         <div className="add-todo">
-          할일 입력: <input className="add-todo-inputer" />
-          <button className="add-todo-inputer-button">입력</button>
+          할일 입력:{' '}
+          <input
+            className="add-todo-inputer"
+            value={this.state.word}
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+          />
+          <button className="add-todo-inputer-button" onClick={this.addTask}>
+            입력
+          </button>
+          <div className="warning">{this.renderWarning(this.state.tasks)}</div>
         </div>
         <div className="todo-list-container">
           해야할 일들
-          <button className="todo-list-hide-button">접기</button>
+          <button className="todo-list-hide-button" onClick={this.hideTask}>
+            접기
+          </button>
           <div className="todo-list">{this.renderTaskDom()}</div>
         </div>
       </div>
