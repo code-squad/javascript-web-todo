8a2f5942663f16556225eb01a83b5df26187fb47
HTMLhead <mrdoo309@gmail.com>
2019-03-26 16:20:26 +0900

    Feat: status-board기능 구현

---
+++ src/js/status-board.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class StatusBoard extends Component {
+  getTaskNum(status, tasks) {
+    if(tasks === null) return;
+    let statusNum = 0;
+    
+    tasks.forEach(v => {
+      if (v.status === status) statusNum++;
+    });
+    return statusNum;
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <div className="status-board-container">
+        <div className={this.props.className}>
+          {this.getTaskNum('todo', this.props.state.tasks)}
+        </div>
+        <div className={this.props.className}>
+          {this.getTaskNum('done', this.props.state.tasks)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default StatusBoard;
