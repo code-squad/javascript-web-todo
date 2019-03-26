d4278b51fc23505ff16031e334a19e35a12fd302
HTMLhead <mrdoo309@gmail.com>
2019-03-26 14:25:30 +0900

    Feat: 서버쪽 코드에서 CORS문제 해결

--- todoServer/route/dataRoute.js
+++ todoServer/route/dataRoute.js
@@ -3,6 +3,9 @@ const jsonData = require('../public/task.json')
 const router = express.Router();
 
 router.get('/', (req, res, next) => { 
+  res.set({
+    'Access-Control-Allow-Origin':'*'
+  })
   res.json(jsonData)
 });
 
