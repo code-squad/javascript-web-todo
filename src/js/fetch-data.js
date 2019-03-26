dafb092b5b741cd355c638effcab51dd948691a2
HTMLhead <mrdoo309@gmail.com>
2019-03-26 15:59:13 +0900

    Style: 세미콜론 밑 공백추가

--- src/js/fetch-data.js
+++ src/js/fetch-data.js
@@ -1,6 +1,6 @@
-const taskDataUrl = 'https://javascript-web-todo-server.herokuapp.com/'
+const taskDataUrl = 'https://javascript-web-todo-server.herokuapp.com/';
 
-const getData = async (dataUrl) => {
+const getData = async dataUrl => {
   try {
     const fetchedRes = await fetch(dataUrl);
     const jsonData = await fetchedRes.json();
@@ -8,6 +8,6 @@ const getData = async (dataUrl) => {
   } catch (err) {
     console.log(err);
   }
-}
+};
 
-export { getData, taskDataUrl }
+export { getData, taskDataUrl };
