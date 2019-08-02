1ce67a908c7cdca0523def9264590d95ff3d7967
leehwarang <hwrng2@gmail.com>
2019-08-03 00:10:54 +0900

    feat: ShowTodo 컴포넌트 생성

--- src/components/ShowTodo.jsx
+++ src/components/ShowTodo.jsx
@@ -1,24 +1,58 @@
-const React = require("react");
-const { Component } = React;
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { display } from "@material-ui/system";
 
 class ShowTodo extends Component {
   constructor(props) {
     super(props);
   }
 
+  makeLi(todos) {
+    const arr = todos.map(data => {
+      return <LI key={data.id}>{data.title}</LI>;
+    });
+
+    return arr;
+  }
+
   render() {
+    const todos = this.props.data;
     return (
       <>
-        <p>해야할 일들</p>
-        <ul>
-          <li>공부하기</li>
-          <li>스터디준비</li>
-          <li>알고리즘공부</li>
-          <li>컴퓨터게임</li>
-        </ul>
+        {todos.length > 0 && (
+          <DIV>
+            <HEADER>해야할 일</HEADER>
+            <Button color="secondary">접기</Button>
+            <ul>{this.makeLi(todos)}</ul>
+          </DIV>
+        )}
       </>
     );
   }
 }
 
-module.exports = ShowTodo;
+export default ShowTodo;
+
+const DIV = styled.div`
+  width: 400px;
+  background-color: #dedcee;
+  border-radius: 30px;
+  margin-top: 80px;
+`;
+
+const LI = styled.li`
+  font-family: Monospace;
+  font-size: 18px;
+  font-weight: 100;
+  height: 40px;
+`;
+
+const HEADER = styled.h3`
+  font-family: Monospace;
+  text-align: center;
+  font-weight: 400;
+  font-size: 20px;
+  padding-top: 10px;
+`;
