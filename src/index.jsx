48cbe83cd6f2d03fef5d9b28adde5fd1b4f12cfc
leehwarang <hwrng2@gmail.com>
2019-08-02 22:47:49 +0900

    setting: htmlm-webpack-plugin 추가 및 devServer 적용

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-const ToDoApp = require("./ToDoApp.jsx");
+import React from "react";
+import ReactDOM from "react-dom";
+import ToDoApp from "./components/ToDoApp";
 
 ReactDOM.render(<ToDoApp />, document.querySelector("#root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   name: "todo-setting",
@@ -7,19 +8,41 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"]
   },
-  entry: "./src/index.jsx",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"), // 이 경로에 있는 파일이 변경될 때 번들을 다시 컴파일
+    compress: true,
+    port: 8080
+  },
+  entry: "./src/index",
   module: {
+    // loader 적용
     rules: [
       {
         test: /\.jsx?/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"]
+        use: {
+          loader: "babel-loader", // js 코드에 주어진 preset을 적용하게 해줌
+          options: {
+            presets: [
+              [
+                "@babel/preset-env", // 최근 js 문법을 사용하게 해주는 preset
+                {
+                  modules: "false" // tree shaking 가능하게 함
+                }
+              ],
+              "@babel/preset-react"
+            ]
+          }
         },
         exclude: /node_modules/
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // template의 html에 output에서 만들어진 bundle.js를 적용하여 html 파일 생성
+      template: `./public/index.html`
+    })
+  ],
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js"
