b20b8797ac6eb7832da08138cca622be2f3b3a9d
fed-gren <deveb22@gmail.com>
2019-08-06 16:01:18 +0900

    polish: edit babel config
    - browserslist package.json에서 제거하고 babel.config.js에 작성

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,14 @@
-const presets = ["@babel/preset-env", "@babel/preset-react"];
+const presets = [
+  [
+    "@babel/preset-env",
+    {
+      targets: {
+        browsers: [">0.25%", "not dead"]
+      }
+    }
+  ],
+  "@babel/preset-react"
+];
 const plugins = ["babel-plugin-styled-components"];
 
 module.exports = { presets, plugins };
