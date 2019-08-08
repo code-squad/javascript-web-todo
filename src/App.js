205d2b660dbe24f54afb5047e9647438cde7290d
sunwook kim <koownusmik@gmail.com>
2019-08-09 06:42:45 +0900

    feat: 외부데이터 받아오고 state로 추가
    1. componentDidMount로 외부API이용해 todo를 추가함,
    
    외부의 데이터를 가져왔으므로 잘못된 url인 경우를 대비해 에러처리를 추가함.
    
    2. TodoTable 컴포넌트의 state가 바뀔때마다 inputBar도 재 랜더링되는것을 방지하기 위해 pureComponent를 사용함.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import React, { Component } from "react";
+import React, { Component, PureComponent } from "react";
 
 export default class TodoTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+
+  async componentDidMount() {
+    // fetch로 받은 데이터를 사용하기 위해서는 response.json()해주어야함.
+    try {
+      const response = await fetch(
+        "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/todolist"
+      );
+      const data = await response.json();
+      if(data.statusCode === '404') throw Error("status404 : 잘못된 url로 요청을 보냈습니다.");
+      const todos = data.body;
+
+      // setState에 ({}) 로 써주기
+      this.setState({
+        todos
+      });
+    } catch (error) {
+      console.warn(error)
+    }
+  }
+
   render() {
-    const todoList = [
-      {
-        title: "공부하기",
-        id: 1233,
-        status: "todo"
-      },
-      {
-        title: "스터디준비",
-        id: 1234,
-        status: "todo"
-      },
-      {
-        title: "알고리즘공부",
-        id: 1230,
-        status: "todo"
-      },
-      {
-        title: "컴퓨터게임",
-        id: 1231,
-        status: "todo"
-      }
-    ];
+    console.log("TodoTable is render...", this.state.todos);
     return (
       <div>
         <InputBar />
-        <OutputTable todoList={todoList} />
+        <OutputTable todoList={this.state.todos} />
       </div>
     );
   }
 }
 
-class InputBar extends Component {
+class InputBar extends PureComponent {
   render() {
+    console.log("inputBar is render...");
     return (
       <form>
         <label>할일 입력 : </label>
@@ -47,6 +53,7 @@ class InputBar extends Component {
 
 class OutputTable extends Component {
   render() {
+    console.log("OutputTable is render...");
     const todoList = this.props.todoList.map(todoItem => (
       <OutputRow key={todoItem.id} title={todoItem.title} />
     ));
