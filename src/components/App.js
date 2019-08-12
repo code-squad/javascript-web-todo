871ac5a0ec868d27197dc161e5cf8af3d3e17b0d
Sangkyu <park78951@gmail.com>
2019-08-12 14:48:14 +0900

    Handle errors by using try catch
    - Change catch from then chains to try-catch to handle errors.
    - Add error.js for error messages to use when error is catched.

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import Input from './Input/Input';
 import TodoList from './TodoList/TodoList';
 import ShowContentsBtn from './ShowButton/ShowContentsBtn';
 import styled from 'styled-components';
-import todoApi from '../Apis/todoApi';
+import todoApi from '../apis/todoApi';
+import { INVALID_DATA, NETWORK_ERROR } from '../message/error';
 
 const Div = styled.div`
   margin: 50px auto;
@@ -16,16 +17,14 @@ class App extends Component {
   }
 
   async fetchMyTodoList()  {
-    await todoApi.get('/develop/todolist')
-      .then(res => {
-        if(res.status === 200) {
-          const myTodoList = res.data.body;
-          this.setState({ todo: myTodoList });
-        } else {
-          return Promise.reject(res.status);
-        }
-      })
-      .catch(reason => console.log(reason.message));
+    try {
+      const todoJson = await todoApi.get('/develop/todolist');
+      const myTodoList = todoJson.data.body;
+      this.setState({ todo: myTodoList });
+    } catch(err) {
+      if(err.name === 'typeError') console.log(INVALID_DATA);
+      if(err.message === 'Network Error') console.log(NETWORK_ERROR);
+    }
   }
 
   componentDidMount() {
