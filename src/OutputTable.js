ff9cd626d05700f325e26faa80225877c5a50e1a
sunwook kim <koownusmik@gmail.com>
2019-08-14 17:19:47 +0900

    feat: OutputTable안의 접기/펼치기 기능 구현
    isOpened의 상태에 따라 다른 css 스타일이 적용되도록 구현

--- src/OutputTable.js
+++ src/OutputTable.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import OutputRow from "./OutputRow";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Background = styled.div`
   background: #a6d0d1;
@@ -11,8 +11,19 @@ const Table = styled.div`
   justify-content: space-between;
 `;
 
+const Ul = styled.ul`
+  ${props => !props.isOpened && css`
+    display:none;
+  `}
+`;
+
 export default function OutputTable(props) {
   const { deleteTodo } = props;
+  const [isOpened, setIsOpened] = useState(true);
+
+  const handleClick = () => {
+    setIsOpened(!isOpened);
+  };
 
   const todoList = props.todoList.map(todoItem => (
     <OutputRow
@@ -28,9 +39,9 @@ export default function OutputTable(props) {
     <Background>
       <Table>
         <h3>해야할 일들</h3>
-        <button>접기</button>
+        <button onClick={handleClick}>접기</button>
       </Table>
-      <ul>{todoList}</ul>
+      <Ul isOpened={isOpened}>{todoList}</Ul>
     </Background>
   );
 }
