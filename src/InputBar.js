aa9213709c66434bd5b0c1701a38424c5c8efb32
sunwook kim <koownusmik@gmail.com>
2019-08-14 13:37:01 +0900

    feat: 새로운 할일 등록 기능 추가
    - hooks 를 순서대로 사용하기 위한 eslint 플러그인추가
    - id 생성용 react-id-genertator 추가
    
    TodoTable 컴포넌트에 추가한것
    1. input에 저장될 newTodo를  state로 등록.
    2. newTodo 를 변경하기 위한 함수 changeInputData 함수도  추가
    3. newTodo가 todos에 추가되도록 하는 addTodo 함수 추가
    4. InputBar에 newTodo, addTodo , changeInputData props로 등록

--- src/InputBar.js
+++ src/InputBar.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from "react";
 import styled from "styled-components";
+import nextId from "react-id-generator";
+
 
 const Form = styled.form`
   background: #a6d0d1;
@@ -11,18 +13,17 @@ const Form = styled.form`
   margin-bottom: 2rem;
 `;
 
+export default function InputBar(props) {
+  // {useId} , nextId 차이를 모르겠음.
+  const id = nextId();
 
-export default function InputBar() {
-  // 당연히 함수니까 어디던 콘솔이 위치해도 상관없음.
-  console.log("inputBar is render...");
+  const { newTodo, changeInputData, addTodo } = props;
 
   return (
     <Form>
-    <label>할일 입력 : </label>
-    <input />
-    <button>등록</button>
+      <label>할일 입력 : </label>
+      <input value={newTodo} onChange={changeInputData} />
+      <button onClick={addTodo} data-id={id} >등록</button>
     </Form>
   );
 }
-
-
--- src/TodoTable.js
+++ src/TodoTable.js
@@ -8,26 +8,53 @@ export default function TodoTable() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    fetchInitialData("https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/todolist");
+    fetchInitialData(
+      "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/todolist"
+    );
   }, []);
 
   const fetchInitialData = async url => {
     try {
-      const response = await fetch(url);  
+      const response = await fetch(url);
       const data = await response.json();
-      if(data.statusCode === '404') throw Error("status404 : 잘못된 url로 요청을 보냈습니다.");
+      if (data.statusCode === "404")
+        throw Error("status404 : 잘못된 url로 요청을 보냈습니다.");
       setTodos([...data.body]);
     } catch (error) {
-      console.warn(error)
+      console.warn(error);
     }
+  };
+
+  const [newTodo, setNewTodo] = useState();
+
+  // onChange 이벤트에 걸리는 핸들러함수, newTodo의 값을 바꿈.
+  const changeInputData = ({ target: { value } }) => {
+    // 여기서 newTodo를 타이틀 값이 아니라 객체로 만들어서 보내면 안되서
+    // addTodo에서 보낸 타이틀 기준으로 만드는것으로 변경
+    setNewTodo(value);
+  };
+  const addTodo = e => {
+    e.preventDefault();
+
+    const newTodoObj = {
+      title: newTodo,
+      id: e.target.dataset.id,
+      status: "todo"
+    };
+
+    setTodos([...todos, newTodoObj]);
+    setNewTodo("");
   };
 
   console.log("TodoTable is render...", todos);
   return (
     <div>
-      <InputBar />
+      <InputBar
+        newTodo={newTodo}
+        addTodo={addTodo}
+        changeInputData={changeInputData}
+      />
       <OutputTable todoList={todos} />
     </div>
   );
 }
-
