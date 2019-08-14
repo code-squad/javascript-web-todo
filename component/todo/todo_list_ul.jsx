f73d13f2e142fb7c3c49454cb08e9ed356327e2b
kkw_10 <kkw_10@naver.com>
2019-08-14 21:21:15 +0900

    기능: fetch 중 loading 메세지 출력 기능 구현

--- component/todo/todo_list_ul.jsx
+++ component/todo/todo_list_ul.jsx
@@ -3,7 +3,7 @@ import ToDoListLi from './todo_list_li';
 import styled from 'styled-components';
 
 const Wrap = styled.div`
-    display: ${props => (props.display ? 'block' : 'none')}
+
 `
 
 const Ul = styled.ul`
@@ -17,30 +17,40 @@ const ToDoListUl = ({ toggle }) => {
 
     const fetchAPI = async () => {
         try {
-            const reqData = await fetch("http://ec2-13-125-97-75.ap-northeast-2.compute.amazonaws.com/todoAPI");
+            const reqData = await fetch("http://ec2-15-164-215-124.ap-northeast-2.compute.amazonaws.com/todoAPI");
             const resData = await reqData.json();
             setApiInfo(resData.body);
-
+            
         } catch (e) {
             console.log(e);
             return;
         }
     }
 
+    const renderList = () => {
+        return apiInfo.length === 0
+            ? <div>loading...</div>
+            : <Ul toggle={toggle}>
+                <ToDoListLi key={'0000'} value={'Study react.'} />
+                {apiInfo.map((v) => {
+                    console.log(v);
+                    return (
+                        <ToDoListLi key={v.id} value={v.title} />
+                    )
+                })}
+            </Ul>              
+    }
+
     useEffect(() => {
-        fetchAPI();
+        setTimeout(() => {
+            fetchAPI();
+        }, 4000)
     }, [])
 
     return (        
-        <Ul toggle={toggle}>
-            <ToDoListLi key={'0000'} value={'Study react.'} />
-            {apiInfo.map((v) => {
-                console.log(v);
-                return (
-                    <ToDoListLi key={v.id} value={v.title} />
-                )
-            })}
-        </Ul>        
+        <Wrap>
+            {renderList()}
+        </Wrap>
     )
 }
 
