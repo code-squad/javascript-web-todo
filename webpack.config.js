d2777ebd7a95274a13f66d1b0fd8a3f0bccf1bb2
allen <revenoir7@gmail.com>
2019-08-17 22:38:40 +0900

    etc: express package설치

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  devtool: 'eval',
+  devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.json', '.jsx', '.css'],
   },
