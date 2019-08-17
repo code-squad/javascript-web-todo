e46375ab6b91717f699016c58ff558de0ae74c2d
allen <revenoir7@gmail.com>
2019-08-17 15:10:24 +0900

    style: 모달창 스타일 추가

--- src/atomicComponents/WarningModal.jsx
+++ src/atomicComponents/WarningModal.jsx
@@ -2,13 +2,36 @@ import React from "react";
 import styled from "styled-components";
 
 const Div = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 50vw;
+  height: 10vh;
   border-radius: 3px;
   border: 2px solid palevioletred;
   font-size: 2rem;
+  background: white;
+`;
+
+const Wrapper = styled.div`
+  position: fixed;
+  background: black;
+  opacity: 0.5;
+  width: 100vw;
+  height: 100vh;
 `;
 
 const WarningModal = props => {
-  return <Div>{props.children}</Div>;
+  return (
+    <>
+      <Wrapper />
+      <Div>{props.children}</Div>;
+    </>
+  );
 };
 
 export default WarningModal;
