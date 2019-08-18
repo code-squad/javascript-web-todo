054169abcd4ad7f6ffc38395e5f185612388b300
allen <revenoir7@gmail.com>
2019-08-18 15:47:51 +0900

    refactor: useInput 커스텀훅  작성
    - input태그의 state를 useInput 훅으로 관리
    - submit이벤트 발생 이후 input의 value 초기화 작업 추가 (TodoInput.jsx:20)

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,30 +1,28 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import Button from "../atomicComponents/Button";
 import Input from "../atomicComponents/Input";
 import { TodoContext } from "./TodoContextProvider";
+import useInput from "./useInput";
 
 const Form = styled.form`
   display: flex;
 `;
 
-//컴포넌트의 의존성에 대한 고민.. TodoContext에 의존함
 const TodoInput = () => {
-  const [newTodo, setNewTodo] = useState();
   const { dispatch } = useContext(TodoContext);
+  const [state, handleChange] = useInput();
+  const { todoInput } = state;
 
   const handleSubmit = e => {
     e.preventDefault();
-    dispatch({ type: "ADD", payload: newTodo });
-  };
-
-  const handleChange = ({ target }) => {
-    setNewTodo(target.value);
+    dispatch({ type: "ADD", payload: todoInput });
+    handleChange({ target: { name: "todoInput", value: "" } });
   };
 
   return (
     <Form onSubmit={handleSubmit} action="/">
-      <Input placeholder="할일을 적어주세요" value={newTodo} onChange={handleChange} />
+      <Input placeholder="할일을 적어주세요" name="todoInput" value={todoInput} onChange={handleChange} />
       <Button>입력</Button>
     </Form>
   );
