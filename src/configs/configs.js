ce96cd9c2e9279d821ee07c1ba94fdab9b2e66ca
allen <revenoir7@gmail.com>
2019-08-18 14:37:29 +0900

    etc: 배포용 url 수정

--- src/configs/configs.js
+++ src/configs/configs.js
@@ -1,6 +1,6 @@
 const CONFIGS = {
   // url: "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/todolist"
-  url: 'https://allen-webTodo.herokuapp.com/todos'
+  url: 'https://allen-webtodo.herokuapp.com/todos'
 }
 
 export default CONFIGS;
