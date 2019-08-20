566c8e8982febe281bd895d4490ade706835a5cb
sunwook kim <koownusmik@gmail.com>
2019-08-21 08:54:51 +0900

    chore : CRA 삭제

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import NewApp from './newApp/NewApp.jsx';
-// 대문자와 jsx 확장자명 신경쓰기 
 import { Normalize } from 'styled-normalize';
 
 
