9cb487e42472c962994ca8f6daa361852f928ccc
sunwook kim <koownusmik@gmail.com>
2019-08-21 09:05:34 +0900

    chore: webpack으로 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import TodoTable from "./TodoTable";
+import { hot } from "react-hot-loader/root";
 
-export default function App() {
+
+const App = props => {
   return (
-    <>
+    <div>
       <TodoTable />
-    </>
+    </div>
   );
-}
+};
+
+// export 할 때 hot 함수를 실행시켜 내보낸다
+export default hot(App);
