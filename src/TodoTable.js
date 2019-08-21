536f3a0bdd440c8a8198dfd7e35f6fd018a54de7
sunwook kim <koownusmik@gmail.com>
2019-08-21 19:43:16 +0900

    refactor: 서버 url 변경

--- src/TodoTable.js
+++ src/TodoTable.js
@@ -64,8 +64,9 @@ export default function TodoTable() {
   };
 
   useEffect(() => {
+    debugger
     fetchInitialData(
-      "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/todolist"
+      "https://dxvinci.github.io/react-todo/todolist.json"
     );
   }, []);
 
