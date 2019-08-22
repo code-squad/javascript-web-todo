e8a7de472e1ab06947beca4e03a37140edc7b920
allen <revenoir7@gmail.com>
2019-08-22 17:27:43 +0900

    etc: hot loader를 react보다 먼저 import하도록 수정
    - styled-component를 이용한 style수정 시에 hot reload안되던 문제 수정

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import { hot } from 'react-hot-loader/root'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from './components/Root'
-import { hot } from 'react-hot-loader/root'
 
 const Hot = hot(Root);
 
