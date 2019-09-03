1cbd78617aec1427a2adca618f549ecc4f6fccaf
sunwook kim <koownusmik@gmail.com>
2019-09-03 15:59:31 +0900

    refactor: json파일 따로 분리후 읽어오도록 변경
    1. fs가 아닌 require로 읽도록함
    2. 굳이 router에서 /list라는 하위 url을 두어 구성 ( 시험하고싶어서 해봄)
    
    - fs로 읽는것의 2가지 장점. require은 동기적으로 읽어옴, 이렇게 지정한 파일은 Json밖에 못읽음 , 근데 귀찮음
    - https://stackabuse.com/reading-and-writing-json-files-with-node-js/

--- server/todoApp.js
+++ server/todoApp.js
@@ -1,6 +1,12 @@
 const express = require('express');
 // 모듈로 분리하기 위해 router라는 변수를 만들고 
 const router = express.Router();
+// fs로 읽는것의 2가지 장점. require은 동기적으로 읽어옴, 이렇게 지정한 파일은 Json밖에 못읽음 , 근데 귀찮음
+// https://stackabuse.com/reading-and-writing-json-files-with-node-js/
+
+const todolist = require("./data/todolist.json");
+
+// const fs = require('fs');
 
 // middleware that is specific to this router
 // router로 요청이 들어왔을때 항상 실행되는 함수 
@@ -18,32 +24,8 @@ router.get('/',function(req,res){
 // http://localhost:3001/heart/about
 router.get('/list',function(req,res){
   // send와 json 차이는 뭐지? 
-  res.send({
-    "statusCode": 200,
-    "body": [
-      {
-        "title": "로컬서버 웹팩에서 데이터 받아오기",
-        "id": 1233,
-        "status": "todo"
-      },
-      {
-        "title": "서버코드 작성하기",
-        "id": 1234,
-        "status": "todo"
-      },
-      {
-        "title": "라우터 404, 스위치 처리하기",
-        "id": 1230,
-        "status": "todo"
-      },
-      {
-        "title": "step20 테스트코드",
-        "id": 1231,
-        "status": "todo"
-      }
-    ]
-  }
-  );
+  //// console.log(todolist); 로 json객체 넘어오는것 확인
+  res.send(todolist)
 
 })
 
