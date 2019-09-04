0da578902659740c498650b4bdd087f886ff6104
leehwarang <hwrng2@gmail.com>
2019-09-04 13:49:59 +0900

    setting: webpack 설정 추가
    - dev, prod에CleanWebpackPlugin 추가
    - prod에만 optimizw-minimize 추가

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,8 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-console.log("난 development");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   name: "todo-setting",
@@ -12,25 +11,24 @@ module.exports = {
     extensions: [".js", ".jsx"]
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"), // 이 경로에 있는 파일이 변경될 때 번들을 다시 컴파일
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 8080,
     historyApiFallback: true
   },
   entry: "./src/index",
   module: {
-    // loader 적용
     rules: [
       {
         test: /\.jsx?/,
         use: {
-          loader: "babel-loader", // js 코드에 주어진 preset을 적용하게 해줌
+          loader: "babel-loader",
           options: {
             presets: [
               [
-                "@babel/preset-env", // 최근 js 문법을 사용하게 해주는 preset
+                "@babel/preset-env",
                 {
-                  modules: "false" // tree shaking 가능하게 함
+                  modules: "false"
                 }
               ],
               "@babel/preset-react"
@@ -52,6 +50,7 @@ module.exports = {
   },
 
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       // template의 html에 output에서 만들어진 bundle.js를 적용하여 html 파일 생성
       template: `./public/index.html`
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const mode = process.env.NODE_ENV;
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 const url =
-  mode === "dev"
+  process.env.NODE_ENV === "dev"
     ? `http://localhost:3000/todos`
     : "https://michelle-todo.herokuapp.com/todos";
 
@@ -18,18 +18,17 @@ module.exports = {
 
   entry: "./src/index",
   module: {
-    // loader 적용
     rules: [
       {
         test: /\.jsx?/,
         use: {
-          loader: "babel-loader", // js 코드에 주어진 preset을 적용하게 해줌
+          loader: "babel-loader",
           options: {
             presets: [
               [
-                "@babel/preset-env", // 최근 js 문법을 사용하게 해주는 preset
+                "@babel/preset-env",
                 {
-                  modules: "false" // tree shaking 가능하게 함
+                  modules: "false"
                 }
               ],
               "@babel/preset-react"
@@ -51,9 +50,8 @@ module.exports = {
   },
 
   plugins: [
-    // new CleanWebpackPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      // template의 html에 output에서 만들어진 bundle.js를 적용하여 html 파일 생성
       template: `./public/index.html`
     }),
     new webpack.DefinePlugin({
